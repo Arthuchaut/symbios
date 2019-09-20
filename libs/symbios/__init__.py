6a90754215ad0f15300eff32ca9bfc30f222bf3b
arthuchaut <arthur.audebrand@gmail.com>
2019-09-20 16:44:04 +0200

    Implemented Middleware class

--- libs/symbios/__init__.py
+++ libs/symbios/__init__.py
@@ -1,3 +1,9 @@
+import aiormq
+
+Props = aiormq.spec.Basic.Properties
+DeliveredMessage = aiormq.types.DeliveredMessage
+ArgumentsType = aiormq.types.ArgumentsType
+
 from .connector import Connector
 from .consumer import Consumer
 from .message import IncMessage, SenMessage
--- libs/symbios/middleware.py
+++ libs/symbios/middleware.py
@@ -6,13 +6,72 @@
 @note    0.1.0 (2019-09-20): Writed the first drafts.
 '''
 
-from typing import List, Awaitable
+from typing import List, Callable, Awaitable, Any
+from abc import ABC
 
-# from . import Symbios
+from . import DeliveredMessage
+from .symbios import Symbios
+from .message import IncMessage
 
 
 class Middleware:
-    # def __init__(self, self_symbios: Symbios):
-    #     ...
-    ...
+    '''Create a middleware from an awaitable.
 
+    Attributes:
+        task (Callable[[Symbions, IncMessage], Awaitable[Any]]): The task to
+            stack in the middleware queue.
+    '''
+
+    def __init__(self, task: Callable[[Symbios, IncMessage], Awaitable[Any]]):
+        '''The Middleware initializer.
+
+        Args:
+            task (Callable[[Symbios, IncMessage], Awaitable[Any]]): The task
+                to stack in the middleware queue.
+        '''
+
+        self.task = task
+
+
+class MiddlewareQueue:
+    '''The middleware queue manager.
+
+    Manage middlewares and run all tasks when a consume event are triggered.
+
+    Attributes:
+        _symbios (Symbios): The Symbios instance.
+        _stack (List[Middleware]): The Middleware queue.
+    '''
+
+    def __init__(self, symbios: Symbios):
+        '''The MiddlewareQueue initializer.
+
+        Args:
+            symbios (Symbios): The Symbios instance.
+        '''
+
+        self._symbios: Symbios = symbios
+        self._stack: List[Middleware] = []
+
+    def append(self, midd: Middleware) -> None:
+        '''Stack the middleware in the queue.
+
+        Args:
+            midd (Middleware): The middleware to stack.
+        '''
+
+        self._stack.append(midd)
+
+    async def run_until_empty(
+        self, message: DeliveredMessage
+    ) -> Awaitable[Any]:
+        '''Run all middlewares in queue.
+        
+        This method have to be called by the aiormq consumer.
+
+        Args:
+            message (DeliveredMessage): The aiormq delivered message.
+        '''
+
+        for midd in self._stack:
+            await midd.task(self._symbios, IncMessage(message))
