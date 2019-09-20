b3bd0d8392e6b856977ca489562adbb1280042f5
arthuchaut <arthur.audebrand@gmail.com>
2019-09-20 13:28:37 +0200

    Implemented the first draft of the project structure

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# symbios
+# Symbios
+
 An asynchronous messaging queue manager based on [aiormq](https://github.com/mosquito/aiormq).
+
+## License
+
+[MIT](LICENSE) license.
---
+++ libs/symbios/__init__.py
@@ -0,0 +1,6 @@
+from .symbios import Symbios
+from .publisher import Publisher
+from .consumer import Consumer
+from .middleware import Middleware
+from .message import IncMessage, SenMessage
+from .connector import Connector
