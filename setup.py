e9ce94b8346ab1ec67ba3ef99a8954cb77462c6d
Arthuchaut <arthur.audebrand@gmail.com>
2019-11-08 19:22:45 +0100

    Added setup for Pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='Symbios',
+    packages=['Symbios'],
+    version='0.1.0',
+    license='MIT',
+    description='A simple asynchronous AMQP library writed for Python.',
+    author='Arthuchaut',
+    author_email='arthuchaut@gmail.com',
+    url='https://github.com/Arthuchaut/symbios',
+    download_url='https://github.com/Arthuchaut/symbios/archive/v1.1.0-dev.tar.gz',
+    keywords=['Rabbitmq', 'Python', 'Async', 'Broker', 'AMQP'],
+    install_requires=['aiormq'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
